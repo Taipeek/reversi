389a793e194da9f3d19da2e811419707066a14ae
Martin Horejš <horejs@uvt.cz>
2017-09-15 14:53:31 -0500

    progress

--- reversi.js
+++ reversi.js
@@ -19,13 +19,7 @@ var state = {
 var ctx;
 
 
-/** @function ApplyMove
- * A function to apply the selected move to the game
- * @param {object} move - the move to apply.
- */
-function applyMove(x, y, move) {
 
-}
 
 /** @function checkForVictory
  * Checks to see if a victory has been actived
@@ -48,7 +42,7 @@ function nextTurn() {
 }
 
 function getFillStyle() {
-    if (state.turn == "w") return "#dac457";
+    if (state.turn == "w") return "#fff";
     return "#111"
 }
 
@@ -56,6 +50,7 @@ function getOpponent() {
     if (state.turn == "w") return "b";
     return "w"
 }
+
 
 /**
  * Funtion to check if placing stone on x y is OK
@@ -68,36 +63,77 @@ function checkDirection(x, y, dir) {
     var pl = state.turn;
     var pathLength = 0;
     var foundOpponent = false;
-    switch (dir){
-        case 0:
-            for(var ny = y;ny > -1;ny--){
-                if(foundOpponent){
-                    if(!state.board[ny][x]) return 0;
-                    if(state.board[ny][x] == op) pathLength++;
-                    if(state.board[ny][x] == pl) return pathLength;
-                }else{
-                    if(!state.board[ny][x]) return 0;
-                    if(state.board[ny][x] == op){
-                        foundOpponent = true;
-                        pathLength++;
-                    }
-                }
-            }
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
 
+    function checkPath() {
+        if (foundOpponent) {
+            if (!state.board[ny][nx]) return 0;
+            if (state.board[ny][nx] == op) pathLength++;
+            if (state.board[ny][nx] == pl) return pathLength;
+        } else {
+            if (!state.board[ny][nx]) return 0;
+            if (state.board[ny][nx] == op) {
+                foundOpponent = true;
+                pathLength++;
+            }
+        }
+    }
+
+    var ret, nx, ny;
+    switch (dir) {
+        case 0:
+            for (ny = y-1, nx = x; ny > -1; ny--) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+        case 1:
+            for (ny = y-1, nx = x+1; ny > -1 && nx < 8; ny--, nx++) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+        case 2:
+            for (ny = y, nx = x+1; ny > -1 && nx < 8; nx++) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+        case 3:
+            for (ny = y+1, nx = x+1; ny < 8 && nx < 8; ny++, nx++) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+        case 4:
+            for (ny = y+1, nx = x; ny < 8 && nx < 8; ny++) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+        case 5:
+            for (ny = y+1, nx = x-1; ny < 8 && nx > -1; ny++, nx--) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+        case 6:
+            for (ny = y, nx = x-1; ny < 8 && nx > -1; nx--) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+        case 7:
+            for (ny = y-1, nx = x-1; ny > -1 && nx > -1; ny--, nx--) {
+                if ((ret = checkPath()) != undefined) return ret;
+            }
+            break;
+            return 0;
     }
 
 }
 
 function getMove(x, y) {
-
-    return null;
+    var path = {dir: -1, length: -1};
+    for (var i = 0; i < 8; i++) {
+        var dirPath = checkDirection(x, y, i);
+        if (dirPath > path.length) {
+            path = {dir: i, length: dirPath};
+        }
+    }
+    return path;
 }
 
 
@@ -111,12 +147,9 @@ function handleHover(event) {
     renderBoard();
     if (!state.board[y][x]) {
         var move = getMove(x, y);
-        if (!move) return;
-
-        ctx.strokeStyle = "yellow";
-        ctx.beginPath();
-        ctx.arc(x * 100 + 50, y * 100 + 50, 40, 0, Math.PI * 2);
-        ctx.fill();
+        if (move.length <1) return renderBoard();
+        renderBoard();
+        renderMove(x, y, move.dir, move.length);
     }
 
 }
@@ -129,10 +162,11 @@ function handleClick(event) {
 function renderBoard() {
     for (var y = 0; y < 8; y++) {
         for (var x = 0; x < 8; x++) {
-            if ((x + y) % 2 == 1) {
-                ctx.fillStyle = "#444";
-                ctx.fillRect(x * 100, y * 100, 100, 100);
-            }
+            ctx.fillStyle = "#56dc56";
+            ctx.strokeStyle = "black";
+            ctx.fillRect(x * 100, y * 100, 100, 100);
+            ctx.strokeRect(x * 100, y * 100, 100, 100);
+
             if (state.board[y][x]) {
                 switch (state.board[y][x]) {
                     case "b":
@@ -143,7 +177,7 @@ function renderBoard() {
                         break;
                     case "w":
                         ctx.beginPath();
-                        ctx.fillStyle = "#dac457";
+                        ctx.fillStyle = "#fff";
                         ctx.arc(x * 100 + 50, y * 100 + 50, 40, 0, Math.PI * 2);
                         ctx.fill();
                         break;
@@ -154,32 +188,106 @@ function renderBoard() {
     }
 }
 
-function renderMove(x, y) {
-    for (var y = 0; y < 8; y++) {
-        for (var x = 0; x < 8; x++) {
-            if ((x + y) % 2 == 1) {
-                ctx.fillStyle = "#444";
-                ctx.fillRect(x * 100, y * 100, 100, 100);
-            }
-            if (state.board[y][x]) {
-                switch (state.board[y][x]) {
-                    case "b":
-                        ctx.beginPath();
-                        ctx.fillStyle = "#111";
-                        ctx.arc(x * 100 + 50, y * 100 + 50, 40, 0, Math.PI * 2);
-                        ctx.fill();
-                        break;
-                    case "w":
-                        ctx.beginPath();
-                        ctx.fillStyle = "#dac457";
-                        ctx.arc(x * 100 + 50, y * 100 + 50, 40, 0, Math.PI * 2);
-                        ctx.fill();
-                        break;
-                }
-            }
+function drawSquarePath(x, y) {
+    ctx.fillStyle = "rgba(255, 0, 0, 0.47)";
+    ctx.fillRect(x * 100, y * 100, 100, 100);
+}
 
-        }
+function renderMove(x, y, dir, length) {
+    var lengthLimit = -2,nx,ny;
+    switch (dir) {
+        case 0:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1); ny--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 1:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1 || nx < 8); ny--, nx++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 2:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1 || nx < 8); nx++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 3:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx < 8); ny++, nx++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 4:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx < 8); ny++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 5:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx > -1); ny++, nx--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 6:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx > -1); nx--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 7:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1 || nx > -1); ny--, nx--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
     }
+}
+
+
+/** @function ApplyMove
+ * A function to apply the selected move to the game
+ * @param {object} move - the move to apply.
+ */
+function applyMove(x, y, dir, length) {
+    var lengthLimit = -2,nx,ny;
+
+    switch (dir) {
+        case 0:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1); ny--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 1:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1 || nx < 8); ny--, nx++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 2:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1 || nx < 8); nx++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 3:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx < 8); ny++, nx++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 4:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx < 8); ny++, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 5:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx > -1); ny++, nx--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 6:
+            for ( ny = y, nx = x; lengthLimit < length && (ny < 8 || nx > -1); nx--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
+        case 7:
+            for ( ny = y, nx = x; lengthLimit < length && (ny > -1 || nx > -1); ny--, nx--, lengthLimit++) {
+                drawSquarePath(nx, ny)
+            }
+            break;
 }
 
 function setup() {
